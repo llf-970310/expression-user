cb4d762d699fc4b55dea37daee1fa01eec820d81
Longfei Li <lf97310@gmail.com>
2020-11-23 19:10:26 +0800

    fix: bind 0.0.0.0 instead of localhost

--- server.py
+++ server.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     # init thrift server
     exam_handler = UserServiceHandler()
     processor = UserService.Processor(exam_handler)
-    transport = TSocket.TServerSocket(host='127.0.0.1', port=9092)
+    transport = TSocket.TServerSocket(host='0.0.0.0', port=9092)
     tfactory = TTransport.TBufferedTransportFactory()
     pfactory = TBinaryProtocol.TBinaryProtocolFactory()
 
